7ac1af3a5c6dd6fb84162035bf2ff5fba0c30dc2
Emerson1220 <74653664+Emerson1220@users.noreply.github.com>
2022-08-01 18:12:51 +0200

    mise en place de l'API Strapi

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    loader: 'default',
+    domains: ['localhost'],
+  },
+};
